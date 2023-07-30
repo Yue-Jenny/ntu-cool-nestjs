b323726b68eddb54bb1eb006c8e804c9a832afdb
jennychiang <jennyc201911@gmail.com>
2023-07-30 23:52:07 +0800

    update: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CourseController } from './course/course.controller';
+import { CourseService } from './course/course.service';
+import { EnrollmentService } from './enrollment/enrollment.service';
+import { EnrollmentController } from './enrollment/enrollment.controller';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    CourseController,
+    EnrollmentController,
+    UserController,
+  ],
+  providers: [AppService, CourseService, EnrollmentService, UserService],
 })
 export class AppModule {}
